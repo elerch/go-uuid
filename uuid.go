8044fced707d129cb71c248294fde1f594612fc0
Emil Lerch <emil@lerch.org>
2016-03-11 13:01:05 -0800

    add tests/code to support RFC 4122

--- uuid.go
+++ uuid.go
@@ -12,6 +12,8 @@ func GenerateUUID() (string, error) {
 	if _, err := rand.Read(buf); err != nil {
 		return "", fmt.Errorf("failed to read random bytes: %v", err)
 	}
+	buf[6] = (buf[6] & 0x0F) | 0x40 // Version: https://tools.ietf.org/html/rfc4122#section-4.1.3
+	buf[8] = (buf[8] & 0x3F) | 0x80 // Variant: https://tools.ietf.org/html/rfc4122#section-4.1.1
 
 	return FormatUUID(buf)
 }
